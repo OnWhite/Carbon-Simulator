55973c42acf8e6066ff2a941794a6c234a54caa8
work <Onwhite@tum>
2025-05-09 08:38:10 -0700

    small changes to make it run

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@
 #SBATCH --qos=high
 #SBATCH --time=168:00:00
 #SBATCH --job-name=ray_train
-cd /nas/ucb/sophialudewig/Carbon-Simulator-minimal/Carbon-Simulator
+cd /nas/ucb/sophialudewig/Carbon-Simulator-minimal
 
 source .venv/bin/activate
 
-PYTHONPATH=. python3 rllib/training_script.py --run_dir rllib/exp/pl1
+PYTHONPATH=. python3 rllib/training_script.py --run_dir rllib/exp/defuat
