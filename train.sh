afedc83ade774eaa522fbf2c870a6691d0702082
work <Onwhite@tum>
2025-05-09 08:50:32 -0700

    changed path and venv name

--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@
 #SBATCH --qos=high
 #SBATCH --time=168:00:00
 #SBATCH --job-name=ray_train
-cd /nas/ucb/sophialudewig/Carbon-Simulator-minimal
+cd /nas/ucb/sophialudewig/Carbon-Simulator-minimal/Carbon-Simulator
 
 source .venv/bin/activate
-cd /Carbon-Simulator
+
 
 PYTHONPATH=. python3 rllib/training_script.py --run_dir rllib/exp/defuat
