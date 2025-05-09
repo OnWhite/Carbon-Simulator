3608b9a90abcecb0c351c273669d488bfabbb8b4
work <Onwhite@tum>
2025-05-09 08:33:08 -0700

    changed path and venv name

--- train.sh
+++ train.sh
@@ -6,6 +6,6 @@
 #SBATCH --job-name=ray_train
 cd /nas/ucb/sophialudewig/Carbon-Simulator-minimal/Carbon-Simulator
 
-source .venv2/bin/activate
+source .venv/bin/activate
 
 PYTHONPATH=. python3 rllib/training_script.py --run_dir rllib/exp/pl1
