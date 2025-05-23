e05e44ca5dbd80b86a7a87594c1822e22f7813f0
work <63966472+OnWhite@users.noreply.github.com>
2025-05-23 16:01:45 -0700

    added wandb

--- Carbon_simulator/foundation/components/Carbon_allocation.py
+++ Carbon_simulator/foundation/components/Carbon_allocation.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import numpy as np
 
 from Carbon_simulator.foundation.base.base_component import (
@@ -240,6 +243,7 @@ class CarbonRedistribution(BaseComponent):
                 "mobile_idx": world.planner.state["mobile_idx"],
                 "settlement_idx": self.world.planner.state["settlement_idx"],
             })
+            self.wandb_log()
         else:
             self.log.append([])
 
@@ -321,3 +325,31 @@ class CarbonRedistribution(BaseComponent):
             return None
         elif self.planner_mode == "active":
             return self.log
+    def wandb_log(self):
+        world = self.world
+        result = {
+            "carbonproject_percentage": world.planner.state["env_idx"],
+            "avg_emission_rate": world.planner.state["average_Er"],
+"emissions_per_agent_mean": np.mean([a.state["Last_emission"] for a in world.agents]),
+            "remained_emissons": world.planner.state["remained_idx"],
+        }
+
+        for a in world.agents:
+            result.update(
+                {
+                    f"agent_{a.idx}/emission_rate": a.state["Carbon_emission_rate"],
+                    f"agent_{a.idx}/volume": a.state["Manufacture_volume"],
+                    f"agent_{a.idx}/emission": a.state["Last_emission"],
+                    f"agent_{a.idx}/inventory": a.state["inventory"]["Carbon_idx"],
+                    f"agent_{a.idx}/escrow": a.state["escrow"]["Carbon_idx"],
+                    f"agent_{a.idx}/labor": a.state["endogenous"]["Labor"],
+                    f"agent_{a.idx}/coin": a.state["inventory"]["Coin"],
+                    f"agent_{a.idx}/research_ability": a.state["Research_ability"],
+                }
+            )
+
+        # Write the result to the log file
+        with open(os.path.join(os.path.dirname(__file__), "..", "..", "Cap_and_Trade_log.json"), "w") as log_file:
+            json.dump(result, log_file)
+
+        return result
