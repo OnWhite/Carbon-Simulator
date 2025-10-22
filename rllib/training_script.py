9555e7c69256e33eddc083fcfbbdadc4acd2a424
work <63966472+OnWhite@users.noreply.github.com>
2025-10-22 17:45:54 +0200

    Adding final analyisis logging

--- rllib/training_script.py
+++ rllib/training_script.py
@@ -498,6 +498,7 @@ if __name__ == "__main__":
                 step_last_ckpt = maybe_save(
                     trainer, result, ckpt_frequency, ckpt_dir, step_last_ckpt
                 )
+
             # Finish up
             logger.info("Completing! Saving final snapshot...\n\n")
             # saving.save_snapshot(trainer, ckpt_dir)
